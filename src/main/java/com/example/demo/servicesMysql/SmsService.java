fc2d429a2d86d233ad48e7cf9c35892e50fe7125
kassinnebie <nnebiekassi12@gmail.com>
2026-05-31 21:45:05 +0200

    Réorganisation des imports dans SmsService et ComportementUtilisateurController pour améliorer la lisibilité du code.

--- src/main/java/com/example/demo/servicesMysql/SmsService.java
+++ src/main/java/com/example/demo/servicesMysql/SmsService.java
@@ -1,12 +1,14 @@
 package com.example.demo.servicesMysql;
 
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 import jakarta.annotation.PostConstruct;
 
 @Service
